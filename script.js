38afb4338c2d0264f7667a0ce20cdc58ca42dc72
Kirito-exe <mannuprakash935@gmail.com>
2026-03-30 06:55:30 +0530

    built a little, but not sure how to move forward, gonna branch now

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-let num1=0;
-let num2=0;
-let oper="";
+let num1=null;
+let num2=null;
+let oper=null;
 function add(a,b){return a+b};
 function sub(a,b){return a-b};
 function multi(a,b){return a*b};
@@ -18,12 +18,21 @@ function operation(a,b,oper){
     }
 }
 let numbers = document.querySelectorAll(".numbers");
+let operators = document.querySelectorAll(".operation")
+let equals = document.querySelector("#equals")
+operarr = Array.from(operators)
 let display = document.querySelector('#display')
 console.log(numbers);
 numarr = Array.from(numbers);
 numarr.forEach((num)=>num.addEventListener("click",function(){
     numStr=num.textContent;
     display.textContent+=numStr;
+}))
+
+operarr.forEach((opr)=>opr.addEventListener("click",function(){
+    num1 = parseFloat(display.textContent);
+    oper = opr.textContent;
+    display.textContent="";
 }))
 
 Clear.addEventListener("click", ()=> display.textContent="");
